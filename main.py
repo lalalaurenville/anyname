f2ef96a122e06ec5034a443fb59c16aad90e6fbc
Mohamed Wane <momow500@gmail.com>
2017-07-25 16:20:02 -0400

    linked pages for events

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 import jinja2
 import os
@@ -44,10 +29,24 @@ class EventsHandler(webapp2.RequestHandler):
         template= jinja_environment.get_template(
             'templates/events.html')
         self.response.write(template.render())
-
+class ConcertsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/concerts.html')
+        self.response.write(template.render())
+class SportsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/sports.html')
+        self.response.write(template.render())
+class MoviesHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/movies.html')
+        self.response.write(template.render())
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/Fashion.html', FashionHandler),
     ('/food', FoodHandler),
-    ('/events.html', EventsHandler)
+    ('/events.html', EventsHandler),
+    ('/concerts.html', ConcertsHandler),
+    ('/sports.html', SportsHandler),
+    ('/movies.html', MoviesHandler)
 ], debug=True)
